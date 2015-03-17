137e091d16fc8302290c8db26909184513cb9e48
Konstantinos Daras <konstantinos.daras@gmail.com>
2015-03-17 01:25:50 +0000

    init test

---
+++ ZDStudio/frmMain.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace ZDStudio {
+  partial class frmMain {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing) {
+      if (disposing && (components != null)) {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent() {
+      System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+      this.ribbon2 = new System.Windows.Forms.Ribbon();
+      this.ribbonTab1 = new System.Windows.Forms.RibbonTab();
+      this.ribbonButton1 = new System.Windows.Forms.RibbonButton();
+      this.ribbonOrbMenuItem1 = new System.Windows.Forms.RibbonOrbMenuItem();
+      this.ribbonOrbMenuItem2 = new System.Windows.Forms.RibbonOrbMenuItem();
+      this.ribbonOrbMenuItem3 = new System.Windows.Forms.RibbonOrbMenuItem();
+      this.ribbonSeparator1 = new System.Windows.Forms.RibbonSeparator();
+      this.ribbonOrbMenuItem4 = new System.Windows.Forms.RibbonOrbMenuItem();
+      this.ribbonOrbMenuItem5 = new System.Windows.Forms.RibbonOrbMenuItem();
+      this.ribbonOrbRecentItem1 = new System.Windows.Forms.RibbonOrbRecentItem();
+      this.ribbonOrbRecentItem2 = new System.Windows.Forms.RibbonOrbRecentItem();
+      this.ribbonOrbOptionButton1 = new System.Windows.Forms.RibbonOrbOptionButton();
+      this.ribbonOrbOptionButton2 = new System.Windows.Forms.RibbonOrbOptionButton();
+      this.ribbonOrbOptionButton3 = new System.Windows.Forms.RibbonOrbOptionButton();
+      this.ribbonPanel1 = new System.Windows.Forms.RibbonPanel();
+      this.ribbonButton2 = new System.Windows.Forms.RibbonButton();
+      this.ribbonTab2 = new System.Windows.Forms.RibbonTab();
+      this.SuspendLayout();
+      // 
+      // ribbon2
+      // 
+      this.ribbon2.Font = new System.Drawing.Font("Segoe UI", 11.25F);
+      this.ribbon2.Location = new System.Drawing.Point(0, 0);
+      this.ribbon2.Minimized = false;
+      this.ribbon2.Name = "ribbon2";
+      // 
+      // 
+      // 
+      this.ribbon2.OrbDropDown.AllowDrop = true;
+      this.ribbon2.OrbDropDown.BorderRoundness = 8;
+      this.ribbon2.OrbDropDown.Location = new System.Drawing.Point(0, 0);
+      this.ribbon2.OrbDropDown.MenuItems.Add(this.ribbonOrbMenuItem1);
+      this.ribbon2.OrbDropDown.MenuItems.Add(this.ribbonOrbMenuItem2);
+      this.ribbon2.OrbDropDown.MenuItems.Add(this.ribbonOrbMenuItem3);
+      this.ribbon2.OrbDropDown.MenuItems.Add(this.ribbonSeparator1);
+      this.ribbon2.OrbDropDown.MenuItems.Add(this.ribbonOrbMenuItem4);
+      this.ribbon2.OrbDropDown.MenuItems.Add(this.ribbonOrbMenuItem5);
+      this.ribbon2.OrbDropDown.Name = "";
+      this.ribbon2.OrbDropDown.OptionItems.Add(this.ribbonOrbOptionButton1);
+      this.ribbon2.OrbDropDown.OptionItems.Add(this.ribbonOrbOptionButton2);
+      this.ribbon2.OrbDropDown.OptionItems.Add(this.ribbonOrbOptionButton3);
+      this.ribbon2.OrbDropDown.RecentItems.Add(this.ribbonOrbRecentItem1);
+      this.ribbon2.OrbDropDown.RecentItems.Add(this.ribbonOrbRecentItem2);
+      this.ribbon2.OrbDropDown.Size = new System.Drawing.Size(527, 295);
+      this.ribbon2.OrbDropDown.TabIndex = 0;
+      this.ribbon2.OrbImage = ((System.Drawing.Image)(resources.GetObject("ribbon2.OrbImage")));
+      this.ribbon2.OrbText = "";
+      // 
+      // 
+      // 
+      this.ribbon2.QuickAcessToolbar.Items.Add(this.ribbonButton1);
+      this.ribbon2.RibbonTabFont = new System.Drawing.Font("Trebuchet MS", 9F);
+      this.ribbon2.Size = new System.Drawing.Size(1072, 123);
+      this.ribbon2.TabIndex = 1;
+      this.ribbon2.Tabs.Add(this.ribbonTab1);
+      this.ribbon2.Tabs.Add(this.ribbonTab2);
+      this.ribbon2.TabsMargin = new System.Windows.Forms.Padding(12, 26, 20, 0);
+      this.ribbon2.Text = "ribbon2";
+      // 
+      // ribbonTab1
+      // 
+      this.ribbonTab1.Panels.Add(this.ribbonPanel1);
+      this.ribbonTab1.Text = "ribbonTab1";
+      // 
+      // ribbonButton1
+      // 
+      this.ribbonButton1.Image = ((System.Drawing.Image)(resources.GetObject("ribbonButton1.Image")));
+      this.ribbonButton1.MaxSizeMode = System.Windows.Forms.RibbonElementSizeMode.Compact;
+      this.ribbonButton1.SmallImage = ((System.Drawing.Image)(resources.GetObject("ribbonButton1.SmallImage")));
+      this.ribbonButton1.Text = "ribbonButton1";
+      // 
+      // ribbonOrbMenuItem1
+      // 
+      this.ribbonOrbMenuItem1.DropDownArrowDirection = System.Windows.Forms.RibbonArrowDirection.Left;
+      this.ribbonOrbMenuItem1.Image = ((System.Drawing.Image)(resources.GetObject("ribbonOrbMenuItem1.Image")));
+      this.ribbonOrbMenuItem1.SmallImage = ((System.Drawing.Image)(resources.GetObject("ribbonOrbMenuItem1.SmallImage")));
+      this.ribbonOrbMenuItem1.Text = "ribbonOrbMenuItem1";
+      // 
+      // ribbonOrbMenuItem2
+      // 
+      this.ribbonOrbMenuItem2.DropDownArrowDirection = System.Windows.Forms.RibbonArrowDirection.Left;
+      this.ribbonOrbMenuItem2.Image = ((System.Drawing.Image)(resources.GetObject("ribbonOrbMenuItem2.Image")));
+      this.ribbonOrbMenuItem2.SmallImage = ((System.Drawing.Image)(resources.GetObject("ribbonOrbMenuItem2.SmallImage")));
+      this.ribbonOrbMenuItem2.Text = "ribbonOrbMenuItem2";
+      // 
+      // ribbonOrbMenuItem3
+      // 
+      this.ribbonOrbMenuItem3.DropDownArrowDirection = System.Windows.Forms.RibbonArrowDirection.Left;
+      this.ribbonOrbMenuItem3.Image = ((System.Drawing.Image)(resources.GetObject("ribbonOrbMenuItem3.Image")));
+      this.ribbonOrbMenuItem3.SmallImage = ((System.Drawing.Image)(resources.GetObject("ribbonOrbMenuItem3.SmallImage")));
+      this.ribbonOrbMenuItem3.Text = "ribbonOrbMenuItem3";
+      // 
+      // ribbonOrbMenuItem4
+      // 
+      this.ribbonOrbMenuItem4.DropDownArrowDirection = System.Windows.Forms.RibbonArrowDirection.Left;
+      this.ribbonOrbMenuItem4.Image = ((System.Drawing.Image)(resources.GetObject("ribbonOrbMenuItem4.Image")));
+      this.ribbonOrbMenuItem4.SmallImage = ((System.Drawing.Image)(resources.GetObject("ribbonOrbMenuItem4.SmallImage")));
+      this.ribbonOrbMenuItem4.Text = "ribbonOrbMenuItem4";
+      // 
+      // ribbonOrbMenuItem5
+      // 
+      this.ribbonOrbMenuItem5.DropDownArrowDirection = System.Windows.Forms.RibbonArrowDirection.Left;
+      this.ribbonOrbMenuItem5.Image = ((System.Drawing.Image)(resources.GetObject("ribbonOrbMenuItem5.Image")));
+      this.ribbonOrbMenuItem5.SmallImage = ((System.Drawing.Image)(resources.GetObject("ribbonOrbMenuItem5.SmallImage")));
+      this.ribbonOrbMenuItem5.Text = "ribbonOrbMenuItem5";
+      // 
+      // ribbonOrbRecentItem1
+      // 
+      this.ribbonOrbRecentItem1.Image = ((System.Drawing.Image)(resources.GetObject("ribbonOrbRecentItem1.Image")));
+      this.ribbonOrbRecentItem1.SmallImage = ((System.Drawing.Image)(resources.GetObject("ribbonOrbRecentItem1.SmallImage")));
+      this.ribbonOrbRecentItem1.Text = "ribbonOrbRecentItem1";
+      // 
+      // ribbonOrbRecentItem2
+      // 
+      this.ribbonOrbRecentItem2.Image = ((System.Drawing.Image)(resources.GetObject("ribbonOrbRecentItem2.Image")));
+      this.ribbonOrbRecentItem2.SmallImage = ((System.Drawing.Image)(resources.GetObject("ribbonOrbRecentItem2.SmallImage")));
+      this.ribbonOrbRecentItem2.Text = "ribbonOrbRecentItem2";
+      // 
+      // ribbonOrbOptionButton1
+      // 
+      this.ribbonOrbOptionButton1.Image = ((System.Drawing.Image)(resources.GetObject("ribbonOrbOptionButton1.Image")));
+      this.ribbonOrbOptionButton1.SmallImage = ((System.Drawing.Image)(resources.GetObject("ribbonOrbOptionButton1.SmallImage")));
+      this.ribbonOrbOptionButton1.Text = "ribbonOrbOptionButton1";
+      // 
+      // ribbonOrbOptionButton2
+      // 
+      this.ribbonOrbOptionButton2.Image = ((System.Drawing.Image)(resources.GetObject("ribbonOrbOptionButton2.Image")));
+      this.ribbonOrbOptionButton2.SmallImage = ((System.Drawing.Image)(resources.GetObject("ribbonOrbOptionButton2.SmallImage")));
+      this.ribbonOrbOptionButton2.Text = "ribbonOrbOptionButton2";
+      // 
+      // ribbonOrbOptionButton3
+      // 
+      this.ribbonOrbOptionButton3.Image = ((System.Drawing.Image)(resources.GetObject("ribbonOrbOptionButton3.Image")));
+      this.ribbonOrbOptionButton3.SmallImage = ((System.Drawing.Image)(resources.GetObject("ribbonOrbOptionButton3.SmallImage")));
+      this.ribbonOrbOptionButton3.Text = "ribbonOrbOptionButton3";
+      // 
+      // ribbonPanel1
+      // 
+      this.ribbonPanel1.Image = ((System.Drawing.Image)(resources.GetObject("ribbonPanel1.Image")));
+      this.ribbonPanel1.Items.Add(this.ribbonButton2);
+      this.ribbonPanel1.Text = "ribbonPanel1";
+      // 
+      // ribbonButton2
+      // 
+      this.ribbonButton2.Image = ((System.Drawing.Image)(resources.GetObject("ribbonButton2.Image")));
+      this.ribbonButton2.SmallImage = ((System.Drawing.Image)(resources.GetObject("ribbonButton2.SmallImage")));
+      // 
+      // ribbonTab2
+      // 
+      this.ribbonTab2.Text = "ribbonTab2";
+      // 
+      // frmMain
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(1072, 624);
+      this.ControlBox = false;
+      this.Controls.Add(this.ribbon2);
+      this.IsMdiContainer = true;
+      this.MaximizeBox = false;
+      this.MinimizeBox = false;
+      this.Name = "frmMain";
+      this.ShowIcon = false;
+      this.Text = "Form1";
+      this.Load += new System.EventHandler(this.frmMain_Load);
+      this.ResumeLayout(false);
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Ribbon ribbon2;
+    private System.Windows.Forms.RibbonOrbMenuItem ribbonOrbMenuItem1;
+    private System.Windows.Forms.RibbonOrbMenuItem ribbonOrbMenuItem2;
+    private System.Windows.Forms.RibbonOrbMenuItem ribbonOrbMenuItem3;
+    private System.Windows.Forms.RibbonSeparator ribbonSeparator1;
+    private System.Windows.Forms.RibbonOrbMenuItem ribbonOrbMenuItem4;
+    private System.Windows.Forms.RibbonOrbMenuItem ribbonOrbMenuItem5;
+    private System.Windows.Forms.RibbonOrbOptionButton ribbonOrbOptionButton1;
+    private System.Windows.Forms.RibbonOrbOptionButton ribbonOrbOptionButton2;
+    private System.Windows.Forms.RibbonOrbOptionButton ribbonOrbOptionButton3;
+    private System.Windows.Forms.RibbonOrbRecentItem ribbonOrbRecentItem1;
+    private System.Windows.Forms.RibbonOrbRecentItem ribbonOrbRecentItem2;
+    private System.Windows.Forms.RibbonButton ribbonButton1;
+    private System.Windows.Forms.RibbonTab ribbonTab1;
+    private System.Windows.Forms.RibbonPanel ribbonPanel1;
+    private System.Windows.Forms.RibbonButton ribbonButton2;
+    private System.Windows.Forms.RibbonTab ribbonTab2;
+
+  }
+}
+
